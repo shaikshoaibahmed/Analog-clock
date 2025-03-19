d0c0bd5d46c98555addcb1b72d468ec7b4998711
shoaibahmed <104643350+shaikshoaibahmed@users.noreply.github.com>
2025-03-19 06:36:49 +0530

    Update analog_clock.py

--- analog_clock.py
+++ analog_clock.py
@@ -34,7 +34,7 @@ canvas = ui.Canvas(window, width=400, height=400, bg="black")
 canvas.pack(expand=True, fill='both')
 
 # create background
-bg = ui.PhotoImage(file='1.jpg')
+bg = ui.PhotoImage(file='1.png')
 canvas.create_image(200, 200, image=bg)
 
 
